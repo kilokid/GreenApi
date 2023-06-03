0f3438e720109861a095402bd7894b4d0014ca84
kilokid <arlomov007@gmail.com>
2023-06-03 19:22:32 +0300

    ✨feat: style for form

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -3,14 +3,18 @@ import './LoginForm.scss';
 const LoginForm = () => {
  return (
     <form className="form">
+      <h2 className='form__title'>Заполните форму</h2>
       <ul className="form__wrapper">
          <li className="form__field">
-            <input type="text" />
+            <label className='form__label' htmlFor="idInstance">Ваш ID *</label>
+            <input className='form__input' type="text" id="idInstance" />
          </li>
          <li className="form__field">
-            <input type="text" />
+            <label className='form__label' htmlFor="apiToken">Ваш токен *</label>
+            <input className='form__input' type="text" id="apiToken" />
          </li>
       </ul>
+      <button className='form__btn' type='button'>Войти</button>
     </form>
  );
 }
